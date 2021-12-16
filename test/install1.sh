af19b7536b710098fbb6b1a3b2e56a61f22e581b
jisoo <ha9873515@naver.com>
2021-12-16 21:21:53 +0900

    3tier

--- test/install1.sh
+++ test/install1.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 setenforce 0
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
-yum install -y httpd
-echo "JISOO-WEB-TEST" > /var/www/html/index.html
-systemctl start httpd
-systemctl enable httpd
+sudo yum install -y httpd
+
+sudo sh -c 'cat >> /etc/httpd/conf/httpd.conf << EOF
+<VirtualHost *:80> 
+ServerName 20.196.248.120
+ErrorLog logs/counterjp.fureweb.com-error_log 
+ProxyRequests Off 
+ProxyPreserveHost On 
+
+<Proxy *> 
+Order deny,allow 
+Allow from all 
+</Proxy> 
+
+ProxyPass /petclinic http://10.0.3.4:8080/petclinic
+ProxyPassMatch ^/(.*\.do)$ http://10.0.3.4:8080/ 
+ProxyPassMatch ^/(.*\.jsp)$ http://10.0.3.4:8080/ 
+ProxyPassReverse / http://10.0.3.4:8080/ 
+</VirtualHost>
+EOF'
+
+sudo systemctl restart httpd
+sudo systemctl enable httpd
